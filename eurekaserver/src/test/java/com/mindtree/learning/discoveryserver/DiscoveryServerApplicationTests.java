2281acb39987bef954da3de7a8293c72e26a224b
srinu34 <srinukiran34@gmail.com>
2021-06-30 13:04:19 +0530

    hello

---
+++ eurekaserver/src/test/java/com/mindtree/learning/discoveryserver/DiscoveryServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mindtree.learning.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
