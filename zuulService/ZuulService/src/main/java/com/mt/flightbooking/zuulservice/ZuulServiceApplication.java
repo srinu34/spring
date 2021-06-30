43253769e838c88b7ce6bfb957455f12f96ced25
srinu34 <srinukiran34@gmail.com>
2021-06-30 12:54:03 +0530

    hello

---
+++ zuulService/ZuulService/src/main/java/com/mt/flightbooking/zuulservice/ZuulServiceApplication.java
@@ -0,0 +1,26 @@
+package com.mt.flightbooking.zuulservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+
+@EnableDiscoveryClient
+@EnableZuulProxy
+@SpringBootApplication
+public class ZuulServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZuulServiceApplication.class, args);
+	}
+
+
+}
+
